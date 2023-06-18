5d05f640fadd1495f6a0401d562eff70303a7665
lin_lin32 <lenajuj@gmail.com>
2023-06-18 23:13:19 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,4 +1,87 @@
 let $document = $(document);
+//tabs start
+(function ($) {
+    /* default option */
+    var defaultOptions = {
+        hash: false,
+        active: 1,
+        afterShow: null
+    };
+    var methods = {
+        init: function (options) {
+            // option
+            options = $.extend({}, defaultOptions, options);
+            var elementA = this.find('a'),
+                containerTab = this.next(),
+                boxTab = this.next().find('.tab-box'),
+                hashWindow = window.location.hash && window.location.hash.replace('-tab', '');
+            /* check hash after load */
+            if (hashWindow && hashWindow.length && options.hash) {
+                setActiveTabLoad('', elementA, boxTab, hashWindow)
+            } else {
+                /* set active tab */
+                setActiveElement(options.active, elementA)
+                setActiveBoxTab(options.active, boxTab)
+            }
+            /* event click on a*/
+            eventHandler(elementA, boxTab, options.hash, options)
+        }
+    };
+    function setActiveElement(active, elementA, id) {
+        if (id) {
+            $(elementA).removeClass('active');
+            $('[href="' + id + '"]').addClass('active');
+        } else {
+            $(elementA).each(function (i, item) {
+                $(item).removeClass('active')
+                if ((i + 1) == active) {
+                    $(item).addClass('active')
+                }
+            })
+        }
+    }
+    function setActiveBoxTab(active, box, id) {
+        if (id) {
+            $(box).removeClass('active').hide();
+            $(id).addClass('active').show();
+        } else {
+            $(box).each(function (i, item) {
+                $(item).removeClass('active').hide();
+                if ((i + 1) == active) {
+                    $(item).addClass('active').show();
+                }
+            })
+        }
+    }
+    function setActiveTabLoad(active, elementA, box, id) {
+        setActiveElement('', elementA, id)
+        setActiveBoxTab('', box, id)
+    }
+    function eventHandler(elementA, box, hash, settings) {
+        elementA.on('click', function () {
+            var activeTab = $(this).attr('href'),
+                _this = this;
+            if (hash) window.location.hash = activeTab + '-tab';
+            elementA.removeClass('active');
+            $(this).addClass('active');
+            setActiveBoxTab('', box, activeTab);
+            if (typeof settings.afterShow === "function") {
+                settings.afterShow(_this, activeTab);
+            }
+            return false
+        })
+    }
+    $.fn.tabArt = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('The ' + method + ' have not');
+        }
+    };
+})(jQuery);
+//tabs end
 (function () {
     "use strict";
     let isMobile = false,
@@ -142,6 +225,10 @@ let $document = $(document);
     };
     genFunc.initialize();
 
+    $('.tab-navigation').tabArt({
+        hash: false,
+        active: 1,
+    });
     /*Function for go to top*/
     $(window).on('scroll', function () {
         let top = $(document).scrollTop();
@@ -314,7 +401,7 @@ let $document = $(document);
         });
     }
     let $page = $('html, body');
-    $('a[href*="#"]').on("click", function() {
+    $('a[href*="#"]').not('.tab-item').on("click", function() {
         $page.animate({
             scrollTop: $($.attr(this, 'href')).offset().top - $(".header").innerHeight()
         }, 400);
